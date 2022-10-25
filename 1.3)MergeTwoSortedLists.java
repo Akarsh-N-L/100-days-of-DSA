770263febe804c3098241e4fc5616e05df8cb5b3
Akarsh-N-L <65647232+Akarsh-N-L@users.noreply.github.com>
2022-10-25 12:09:41 +0530

    Update 1.3)MergeTwoSortedLists.java

--- 1.3)MergeTwoSortedLists.java
+++ 1.3)MergeTwoSortedLists.java
@@ -4,10 +4,10 @@ class Solution{
         else if(h2==null) return h1;
         else{
             if(h1.val<h2.val){
-                h1 = mergeTwoLists(h1.next,h2);
+                h1.next = mergeTwoLists(h1.next,h2);
                 return h1;
             }else{
-                h2 = mergeTwoLists(h1,h2.next);
+                h2.next = mergeTwoLists(h1,h2.next);
                 return h2;
             }
         }
