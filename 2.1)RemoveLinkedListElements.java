17639707cc8cda96705b154b107a0fb5c6845f4b
Akarsh-N-L <65647232+Akarsh-N-L@users.noreply.github.com>
2022-10-26 23:45:22 +0530

    Create 2.1)RemoveLinkedListElements.java

---
+++ 2.1)RemoveLinkedListElements.java
@@ -0,0 +1,8 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) return head;
+        else if(head.val == val) return removeElements(head.next,val);
+        else head.next = removeElements(head.next,val);
+        return head;
+    }
+}
